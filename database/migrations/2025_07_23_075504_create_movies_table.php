28dd4c80b788e9430509d51c1cd12ae8024297e1
Nur Azlin Liana binti Mohd Adlan <azlinliana.adlan@gmail.com>
2025-07-24 15:24:31 +0800

    Create movie table

---
+++ database/migrations/2025_07_23_075504_create_movies_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('movies', function (Blueprint $table) {
+            $table->id();
+            $table->string('name_movie');
+            $table->string('genre_movie');
+            $table->integer('duration_movie');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('movies');
+    }
+};
